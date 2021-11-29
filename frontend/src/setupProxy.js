8f988b41aff21592db6b932ecd4d861ded94d49d
Kim Giyeong <ggcc503@gmail.com>
2021-11-29 11:52:08 +0900

    Change client and server port
    - client port 3000 -> 3001
    - server port 8080 -> 8081

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://localhost:8081",
       changeOrigin: true,
       pathRewrite: {
         "^/api": ""
